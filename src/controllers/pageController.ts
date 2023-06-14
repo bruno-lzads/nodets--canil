b1d2fe81b583db4675b9aefe7b482503a03aba9a
Bruno Luiz <bruno.luiz.3110@gmail.com>
2023-06-14 20:48:00 -0300

    projeto final 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,22 +1,52 @@
 import { Request, Response } from "express";
+import { creatObjectMenu } from "../helpers/creatObjectMenu";
+import { Pet } from '../models/pet'
 
 export const home = (req: Request, res: Response)=>{
-    res.send('home no controller!')
-    //res.render('pages/page')
+    let list = Pet.getAll;
+    res.render('pages/page', {
+        menu: creatObjectMenu('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 
 }
 export const dogs = (req: Request, res: Response)=>{
-
-    //res.render('pages/page')
+    let list = Pet.getFromType('dog');
+    res.render('pages/page', {
+        menu: creatObjectMenu('dog'),
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
 
 }
 export const cats = (req: Request, res: Response)=>{
-
-    //res.render('pages/page')
+    let list = Pet.getFromType('cat');
+    res.render('pages/page', {
+        menu: creatObjectMenu('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
 
 }
 export const fishes = (req: Request, res: Response)=>{
-
-    //res.render('pages/page')
+    let list = Pet.getFromType('fish');
+    res.render('pages/page', {
+        menu: creatObjectMenu('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
 
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
+import { creatObjectMenu } from "../helpers/creatObjectMenu";
+import { Pet } from '../models/pet'
 
 export const search = (req: Request, res: Response)=>{
+    let query: string = req.query.q as string;
+    let list = Pet.getFromName(query);
 
-    //res.render('pages/page')
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('pages/page',{
+        menu: creatObjectMenu(''),
+        list,
+        query
+    })
 
 }
